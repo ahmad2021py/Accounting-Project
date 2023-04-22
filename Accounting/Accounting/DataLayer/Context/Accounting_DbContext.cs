fae2c71fc4244c6d37f9b966b68eae99d93cf144
Ahmad <ahmadbh.mail@gmail.com>
2023-04-22 05:26:45 -0700

    Add all DBsets to Dbcontext         db context list :         1.Categories         2.Company         3.Config         4.User         5.Customer         6.Stock         7.Registration         8.Product

--- Accounting/Accounting/DataLayer/Context/Accounting_DbContext.cs
+++ Accounting/Accounting/DataLayer/Context/Accounting_DbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Accounting.DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,7 +10,16 @@ namespace Accounting.DataLayer.Context
 {
    public class Accounting_DbContext : DbContext
     {
-
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Company> Company { get; set; }
+        public virtual DbSet<Config> Config { get; set; }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Stock> Stock { get; set; }
+        public virtual DbSet<Sales> Sales { get; set; }
+        public virtual DbSet<Registration> Registration { get; set; }
+        public virtual DbSet<ProductSold> ProductSold { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
 
 
 
