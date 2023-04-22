b5a13ed480ce75a153394ba8fc5cd941260aa61b
Ahmad <ahmadbh.mail@gmail.com>
2023-04-22 05:56:05 -0700

    Create UnitOfWork & add IUserRepository in it

---
+++ Accounting/Accounting/DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using Accounting.DataLayer.Interfaces;
+using Accounting.DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Context
+{
+  public  class UnitOfWork
+    {
+        // ############# فیلد های کلاس  ##########
+        Accounting_DbContext db = new Accounting_DbContext();
+
+        private IUserRepository _userRepository;
+
+
+        //#############پراپرتی های کلاس#############
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if (_userRepository == null)
+                {
+                    _userRepository = new UserRepository(db);
+                }
+
+                return _userRepository;
+            }
+        }
+
+        //--------------------------------------------------
+
+
+        //############متد های کلاس ##################
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
+
+
+
+    
